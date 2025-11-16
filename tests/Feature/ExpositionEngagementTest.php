2d77244c1cdd9df09409e5c376eb7a015c353049
lluca1 <ludosanuluca@gmail.com>
2025-11-16 12:32:48 +0200

    refactor ExpositionEngagementTest to use Pest functions and ensure exposition is public

--- tests/Feature/ExpositionEngagementTest.php
+++ tests/Feature/ExpositionEngagementTest.php
@@ -1,15 +1,20 @@
 <?php
 
+/** @var \Tests\TestCase $this */
+
 use App\Livewire\ExpositionExhibits;
 use App\Models\Exposition;
 use App\Models\User;
 use Livewire\Livewire;
+use function Pest\Laravel\actingAs;
+use function Pest\Laravel\assertDatabaseHas;
+use function Pest\Laravel\assertDatabaseMissing;
 
 it('allows an authenticated user to like and unlike an exposition', function () {
     $user = User::factory()->create();
-    $exposition = Exposition::factory()->create();
+    $exposition = Exposition::factory()->create(['is_public' => true]);
 
-    $this->actingAs($user);
+    actingAs($user);
 
     $component = Livewire::test(ExpositionExhibits::class, ['exposition' => $exposition]);
 
@@ -24,15 +29,15 @@ it('allows an authenticated user to like and unlike an exposition', function () 
 
 it('allows a curator to post and delete comments', function () {
     $user = User::factory()->create();
-    $exposition = Exposition::factory()->create();
+    $exposition = Exposition::factory()->create(['is_public' => true]);
 
-    $this->actingAs($user);
+    actingAs($user);
 
     Livewire::test(ExpositionExhibits::class, ['exposition' => $exposition])
         ->set('commentBody', 'Stunning work!')
         ->call('postComment');
 
-    $this->assertDatabaseHas('exposition_comments', [
+    assertDatabaseHas('exposition_comments', [
         'exposition_id' => $exposition->id,
         'user_id' => $user->id,
         'body' => 'Stunning work!',
@@ -43,7 +48,7 @@ it('allows a curator to post and delete comments', function () {
     Livewire::test(ExpositionExhibits::class, ['exposition' => $exposition])
         ->call('deleteComment', $commentId);
 
-    $this->assertDatabaseMissing('exposition_comments', [
+    assertDatabaseMissing('exposition_comments', [
         'id' => $commentId,
     ]);
 });
