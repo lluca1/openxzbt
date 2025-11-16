df95843272c9cc28364511a4d1fe9f49266b9830
lluca1 <ludosanuluca@gmail.com>
2025-11-16 08:05:34 +0200

    better fluff

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -178,7 +178,7 @@
                     {{-- COLORED STATUS TAGS --}}
                     <div class="flex flex-wrap justify-center gap-3">
                         <span class="px-3 py-1 border border-[#38bdf8]/70 bg-[#072635] rounded-none">
-                            endpoint: /expositions
+                            GET endpoint: /api/expositions/{expo_id}
                         </span>
 
                         <span class="px-3 py-1 border border-[#22c55e]/70 bg-[#052713] rounded-none">
@@ -190,7 +190,7 @@
                         </span>
 
                         <span class="px-3 py-1 border border-[#f97373]/70 bg-[#5b1010] rounded-none">
-                            unity_client_required
+                            unity client required
                         </span>
                     </div
                 </div>
